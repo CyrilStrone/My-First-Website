207279a934ae7f3f793d80d09d55fae66a2fdb5e
Cyril <kidvos@gmail.com>
2021-11-04 03:33:48 +0700

    Добавил плагин jquery и он работает

---
+++ JS/jquery.icheck.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_minimal-purple',
+    radioClass: 'iradio_minimal-purple',
+    increaseArea: '50%' // не обязательно
+  });
+});
