10e9e6b1d320544d3cded73821aaa38fa0c886d3
Cyril <kidvos@gmail.com>
2021-11-04 23:11:47 +0700

    Jquery все сломал, сделал все с css

---
+++ JS/itogglermy.js
@@ -0,0 +1,14 @@
+$(function(){
+     
+    $('.jtoggler').jtoggler();
+   
+});
+$(document).on('jt:toggled',function(event, target) {
+    console.log(event, target);
+    console.info($(target).prop('checked'))
+});
+  $(document).on('jt:toggled:multi',function (event, target) {
+    console.log(event, target);
+    console.info($(target).parent().index())
+});
+  
