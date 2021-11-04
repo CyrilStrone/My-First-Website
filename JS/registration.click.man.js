69ba824cfee92203ddafd15f6eca5ccc71040b8c
Cyril <kidvos@gmail.com>
2021-11-05 03:20:37 +0700

    Пытаюсь сделать красивый календарб
    Сделал переключение пола нормальным

---
+++ JS/registration.click.man.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#registration_radio_man").click(function(){
+      $(".registration-checkbox-sex-man").css("color","#1F2041BF");
+    }); 
+   });
---
+++ JS/registration.click.woman.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("#registration_radio_woman").click(function(){
+    $(".registration-checkbox-sex-woman").css("color","#1F2041BF");
+  }); 
+ });
