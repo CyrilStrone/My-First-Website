a417d7eabdd4cf3fb6eb9b92baa18b5f3c2ac9bb
Cyril <kidvos@gmail.com>
2021-12-22 00:35:10 +0700

    БЭМ
    хэдэр футер регистрэйшен

--- JS/registration.click.man.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $("#registration_radio_man").click(function(){
-      $(".registration-checkbox-sex-man").css("color","#1F2041BF");
-    }); 
-   });
--- JS/registration.click.woman.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $("#registration_radio_woman").click(function(){
-    $(".registration-checkbox-sex-woman").css("color","#1F2041BF");
-  }); 
- });
